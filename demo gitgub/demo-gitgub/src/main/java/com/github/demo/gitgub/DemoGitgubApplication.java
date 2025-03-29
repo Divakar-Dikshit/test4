03c67a6c65483d4375104085d6d2005d981717ff
Divakar Dikshit <dcdivakar2000@gmail.com>
2025-03-29 13:31:49 +0530

    first mesg

---
+++ demo gitgub/demo-gitgub/src/main/java/com/github/demo/gitgub/DemoGitgubApplication.java
@@ -0,0 +1,13 @@
+package com.github.demo.gitgub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitgubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitgubApplication.class, args);
+	}
+
+}
